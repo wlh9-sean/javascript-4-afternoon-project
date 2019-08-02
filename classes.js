40827c89b9b9343bf56995ad7cdf6a3d902636a7
Sean Schouten <seanmschouten@gmail.com>
2019-08-02 15:00:35 -0600

    Completed Classes

--- classes.js
+++ classes.js
@@ -30,7 +30,19 @@
 */
 
 //Code Here
+class Employee {
+  constructor(first_name, last_name, email, age){
+    this.first_name = first_name
+    this.last_name = last_name
+    this.email = email
+    this.age = age
+  }
 
+  makeWidget(){
+    return `${this.first_name} ${this.last_name} Widget`
+  }
+
+}
 
 ////////// PROBLEM 2 //////////
 
@@ -49,6 +61,22 @@
 
 //Code Here
 
+class Manager extends Employee {
+  constructor(first_name, last_name, email, age){
+    super(first_name, last_name, email, age)
+
+    this.reports = []
+  }
+
+  hire(employee) {
+    this.reports.push(employee)
+  }
+
+  fire(index){
+    this.reports.splice(index, 1)
+  }
+
+}
 
 ////////// PROBLEM 3 //////////
 
@@ -72,6 +100,52 @@
 */
 
 //Code Here
+
+class ProgressiveManager extends Manager {
+  constructor(first_name, last_name, email, age){
+    super(first_name, last_name, email, age)
+
+    this.title = 'Not a manager'
+    this.bonus = 0
+  }
+
+  hire(employee){
+    super.hire(employee)
+
+    if(this.reports.length === 0){
+      this.title = 'Not a Manager'
+    } if (this.reports.length >= 1 && this.reports.length <= 3){
+      this.title = 'Barely Manager'
+    } if(this.reports.length >= 4 && this.reports.length <= 10){
+      this.title = 'Mostly Manager'
+    } if(this.reports.length >= 11 && this.reports.length <= 50){
+      this.title = 'Manager'
+    } if(this.reports.length >= 51 && this.reports.length <= 100){
+      this.title = 'Manager Plus'
+    } if(this.reports.length >= 101){
+      this.title = 'Bestest Manager'
+    }
+  }
+
+  fire(index){
+    super.fire(index)
+
+    if(this.reports.length === 0){
+      this.title = 'Not a Manager'
+    } if (this.reports.length >= 1 && this.reports.length <= 3){
+      this.title = 'Barely Manager'
+    } if(this.reports.length >= 4 && this.reports.length <= 10){
+      this.title = 'Mostly Manager'
+    } if(this.reports.length >= 11 && this.reports.length <= 50){
+      this.title = 'Manager'
+    } if(this.reports.length >= 51 && this.reports.length <= 100){
+      this.title = 'Manager Plus'
+    } if(this.reports.length >= 101){
+      this.title = 'Bestest Manager'
+    } 
+    this.bonus += 100
+  }
+}
 
 
 
